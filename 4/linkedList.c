f6d337479f0f1f8dfa8b43fee2de9e5a8dc01d52
Erik Gaida <gaidaerik2000@gmail.com>
2025-03-07 13:36:23 +0100

    Update: Code zum rev von verketten Listen

--- 4/linkedList.c
+++ 4/linkedList.c
@@ -1,29 +1,70 @@
-#include <cstddef>
 #include <stdio.h>
 #include <stdlib.h>
-struct glied {
+#include <string.h>
+struct Node {
     int wert;
-    struct glied *next;
+    struct Node* next;
 };
 
+
+void listprinter(struct Node *head){
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d", temp->wert);
+        if(temp->next != NULL){
+            printf(" -> ");
+        } else {
+         printf("\n");
+        }
+        temp = temp -> next;
+    }
+}
 int main(){
-    int zahl;
-    struct glied *kopf = NULL;
+
+    int zahl;                   // speicher für die eingelesene Zahl
+    struct Node *head = NULL;   // zeiger fürs erste element
+    struct Node *neu = NULL;    // zeigt aufs neue element
+
     printf("gib eine zahl ein, -1 bendet die eingabe ");
-    while (scanf("%d",&zahl) == 1 && zahl != -1) {
-        struct glied *neu = malloc(sizeof(struct glied));
-        if(neu == NULL){
-            printf(stderr, "Fehler: Speicherallokierung fehlgeschlagen.\n");
-            while (kopf != NULL) {
-                struct glied *temp = kopf;
-                kopf = kopf->next;
-                free(temp);
-            }
+    while (scanf("%d",&zahl) == 1 && zahl != -1) { // liest elem ein bis anzahl der 
+        neu = malloc(sizeof(struct Node)); // speicher alkoation 
+        if(neu == NULL){    //fehler behandlung falls kein speicher belegt werden konnte.
+            fprintf(stderr, "Fehler: Speicherallokierung fehlgeschlagen.\n");
             return 1;
         }
 
-        neu -> wert = zahl;
-        neu -> next = kopf;
-    
-    }    
+        neu -> wert = zahl; // wert in neu speichern
+        neu -> next = NULL; // next mit null setzen
+
+        if(head == NULL){   // falls head lleer
+            head = neu;
+
+        } else {    
+            struct Node *current = head; // zeiger um durch liste zu gehen
+            while (current->next != NULL){ // bedingung damit wir abbrechen wenn lsite leer
+                current = current->next;
+            }
+            current -> next = neu;
+        }
+
+    }
+    // printed die liste aus
+    listprinter(head);
+
+    // revers liste
+
+    struct Node *prev = NULL;
+    struct Node *current = head;
+    struct Node *next = NULL;
+
+    while (current != NULL) {
+        next = current -> next; 
+        current -> next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+
+    listprinter(head);
+    return 0;
 }
